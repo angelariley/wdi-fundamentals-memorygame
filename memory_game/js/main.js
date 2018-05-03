d099f6ee3f0315356f1e7acee411a8ee2350e355
Riley <AR89400@AD1.PROD>
2018-05-03 18:09:39 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,6 @@
+//Don't forget clean up code prior to submitting on Friday. Double check html error c%7  is correct.
+
 var cards = [
-    {
-    rank: 'queen',
-    suit: 'hearts',
-    cardImage: 'images/queen-of-hearts.png'
-    },
     {
     rank: 'queen',
     suit: 'diamonds',
@@ -15,32 +12,35 @@ var cards = [
     cardImage: 'images/king-of-hearts.png'
     },
     {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+    },
+    {
     rank: 'king',
     suit: 'diamonds',
     cardImage: 'images/king-of-diamonds.png'
     }
     ];
-    
- 
+    var flipCard = function () {
+        cardId = this.getAttribute('data-id');
+        console.log(cardId);
+        cardsInPlay.push(cards[cardId].rank);
+        this.setAttribute('src', cards[cardId].cardImage);
+        if (cardsInPlay.length === 2) {
+        checkForMatch();
+        cardsInPlay = [];
+        }
+        };
     var cardsInPlay = [];
     var checkForMatch = function () {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+    alert("YOU WIN! Click OK to Try Again!");location.reload();
     } else {
-    alert("Sorry, try again.");
+    alert("Wrong Card :( Don't Worry. Click OK to Try Again!");location.reload();
     }
-    };
-    
-    var flipCard = function () {
-    cardId = this.getAttribute('data-id');
-    console.log(cardId);
-    cardsInPlay.push(cards[cardId].rank);
-    this.setAttribute('src', cards[cardId].cardImage);
-    if (cardsInPlay.length === 2) {
-    checkForMatch();
-    cardsInPlay = [];
-    }
-    };
+    }; 
+
     var createBoard = function () {
     for (var i = 0; i < cards.length; i++) {
     var cardElement = document.createElement('img');
@@ -50,5 +50,5 @@ var cards = [
     document.getElementById('game-board').appendChild(cardElement);
 }  
     }
-    createBoard();
     
+    createBoard();
